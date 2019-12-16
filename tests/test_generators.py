c26a6ce8dd2c91391fb071e3117372167ee3042a
Vlad Dmitrievich <me@2tunnels.com>
2019-12-16 16:42:07 +0200

    add adjectives + add get_random_name method

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -5,7 +5,7 @@ from geek.providers import MarvelProvider
 
 
 class TestGenerator:
-    def test_no_provider_added(self):
+    def test_get_random_character_no_provider_added(self):
         generator = Generator(1)
 
         with pytest.raises(RuntimeError) as excinfo:
@@ -18,3 +18,17 @@ class TestGenerator:
         generator.add_provider(MarvelProvider)
 
         assert generator.get_random_character() == 'venus-dee-milo'
+
+    def test_get_random_name_no_provider_added(self):
+        generator = Generator(1)
+
+        with pytest.raises(RuntimeError) as excinfo:
+            generator.get_random_name()
+
+        assert str(excinfo.value) == 'No provider added'
+
+    def test_get_random_name(self):
+        generator = Generator(1)
+        generator.add_provider(MarvelProvider)
+
+        assert generator.get_random_name() == 'competent-kang'
